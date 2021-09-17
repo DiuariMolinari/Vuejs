fb770a5fd01f6b3baffe644f06d7b2d315ec1bab
Diuari.molinari <Diuari.molinari@gmail.com>
2021-09-17 01:43:07 -0300

    Sort grid

--- src/components/SimpleGrid.vue
+++ src/components/SimpleGrid.vue
@@ -1,31 +1,40 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-4 col-lg-4" >
+            <div class="col-3 col-lg-8" >
                 <h2>Pessoas</h2>
+                <input type="submit" value="Sortear Pessoa" class="btn btn-warning" @click="sortPerson">
                 <table class="table">
                     <thead>
                         <tr>
                             <th scope="col">Id</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
+                            <th id="tdName" scope="col" @click="orderByName()">Name 
+                                <fa :icon="iconName" id="faIconName"/>
+                            </th>
+
+                            <th id="tdEmail" :v-if="orderEmailIcon" scope="col" @click="orderByEmail()">Email
+                                <fa  :icon="iconEmail" id="faIconEmail"/>
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for=" p in people" v-bind:key="p.id">
-                            <th scope="row"> {{ p.id }} </th>
+                        <tr v-for="p in people" v-bind:key="p.id" :class="[ p.id == personDrawn.id ? 'selectRow' : '']">
+                            <th scope="row" >  {{ p.id }} </th>
                             <td> {{ p.name }} </td>
                             <td> {{ p.email }} </td>
+                        </tr>
+                        <tr class="totalizer" col>
+                            <!-- <td colspan="3">  Total: {{ people.length }}</td> -->
                         </tr>
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
-    <input type="hidden" id="sequenceId">
 </template>
 
 <script>
+import _ from "lodash";
 export default {
      mounted() { 
             this.eventBus.on("addPerson", (person) => {
@@ -34,18 +43,21 @@ export default {
     },
     data() {
         return {
-            people: [],
+            people: new Array(),
+            personDrawn: {},
+            iconEmail: "arrow-up",
+            iconName: "arrow-up",
+            sequence: 0,
         }
     },
     methods:  {
         addPerson(personAdd) {
-            let newPerson = { id: this.getSequence(), name: personAdd.name, email: personAdd.email }
+            let newPerson = { id: this.getSequence(), name: personAdd.name, email: personAdd.email };
              if (this.validatePerson(newPerson))
                 this.people.push(newPerson);
         },
         validatePerson(person) {
             let messageErros = "";
-            
 
             if (this.people.find(p => p.name === person.name) != null) {
                 messageErros += "Pessoa já cadastrada!\r\n";
@@ -61,17 +73,45 @@ export default {
             alert(message);
         },
         getSequence() {
-            let sequence = document.getElementById("sequenceId");
-            if (sequence.value == "")
-                sequence.value = 0
-           
-            return sequence.value++;
+            this.sequence = this.sequence + 1;
+            return this.sequence;
+        },
+        sortPerson() {
+            this.personDrawn =  this.people[Math.floor(Math.random() * this.people.length)];
+        },
+        orderByEmail() {
+            this.iconEmail = (this.iconEmail == "" || this.iconEmail == null  || this.iconEmail == "arrow-down") ? "arrow-up" : "arrow-down";
+            this.people = this.SortAsc(this.people);
+
+        },
+         orderByName() {
+            this.iconName = (this.iconName == "" || this.iconName == null  || this.iconName == "arrow-down") ? "arrow-up" : "arrow-down";
+            this.people = this.SortAsc(this.people);
+        },
+        SortAsc() {
+            let array = _.cloneDeep(this.people);
+            console.log(array)
+             array= _.orderBy(this.array, name, 'asc');
+            console.log(array)
+        },
+        SortDesc(){
+            let array = _.cloneDeep(this.people);
+            console.log(array)
+             array= _.orderBy(this.array, name, 'asc');
+            console.log(array)
         }
-    }
+    },
 }
 
 </script>
 
 <style>
+.selectRow {
+  background-color: yellow;
+}
+
+.totalizer {
+  background-color: rgb(207, 210, 228)
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import mitt from 'mitt'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas);
 
 const eventBus = mitt()
 const app = createApp(App)
 
+
 app.config.globalProperties.eventBus = eventBus
-app.mount('#app')
+app.component('fa', FontAwesomeIcon).mount('#app')
