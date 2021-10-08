c3f0a65c01fe599f5a841ba487cd8f4864055e3a
Diuari.Molinari <diuari.molinari@benner.com.br>
2021-10-08 00:51:39 -0300

    Prototipo da tela de cadastro de pedidos e produtos

--- src/components/requests/FormRequest.vue
+++ src/components/requests/FormRequest.vue
@@ -1,29 +1,26 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-4 col-lg-4" >
-                <h2>Cadastro de Pedidos</h2>
-                <form @submit.prevent="adicionarPedido" >
-                        <div class="form-group">
-                            <label for="requestNumber">Número pedido</label>
-                            <input type="number" class="form-control" name="requestNumber" id="requestNumber" v-model="request.number"  autofocus required>
-                        </div>
-                        <div class="form-group">
-                            <label for="emitDate">Data de emissão</label>
-                            <input type="date" class="form-control" name="emitDate" id="emitDate" v-model="request.emitDate" required >
-                        </div>
-                    <div class="form-group">
-                        <label for="client">Cliente</label>
-                        <select class="form-control" name="client" id="client" v-model="request.client" required >
-                            <option value="">Teste</option>
-                        </select>
-                    </div>
-                    
-                    <button v-if="this.isAdd" type="submit" class="btn btn-primary" @click="addRequest">Adicionar</button>
-                    <button v-else type="submit" class="btn btn-primary" @click="changeRequest">Alterar</button>
-                </form>
+        <h2>Cadastro de Pedidos</h2>
+        <form @submit.prevent="adicionarPedido" >
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <label for="requestNumber">Número pedido</label>
+                    <input type="number" class="form-control" name="requestNumber" id="requestNumber" v-model="request.number"  autofocus required>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="emitDate">Data de emissão</label>
+                    <input type="date" class="form-control" name="emitDate" id="emitDate" v-model="request.emitDate" required >
+                </div>
             </div>
-        </div>
+            <div class="form-row">
+                 <div class="form-group col-md-8">
+                    <label for="client">Cliente</label>
+                    <select class="form-control" name="client" id="client" v-model="request.client" required >
+                        <option value="">Teste</option>
+                    </select>
+                </div>
+            </div>
+        </form>
     </div>
 </template>
 
--- src/components/requests/GridRequest.vue
+++ src/components/requests/GridRequest.vue
@@ -1,5 +1,42 @@
 <template>
-    <form-request></form-request>  
+    <div class="container">
+        <form-request></form-request>  
+        <div class="btnGroup">
+            <input class="btn btn-primary" type="button" value="Adicionar Produto">
+            <input class="btn btn-info"    type="button" value="Editar">
+            <input class="btn btn-danger"  type="button" value="Excluir">
+        </div>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th id="tdName"  scope="col" class="column">Produto</th>
+                    <th id="tdEmail" scope="col" class="column">Quantidade</th>
+                    <th id="tdEmail" scope="col" class="column">Preço Unitário</th>
+                    <th id="tdEmail" scope="col" class="column">Valor de Desconto</th>
+                    <th id="tdEmail" scope="col" class="column">Total</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="p in request.products" v-bind:key="p.id" >
+                    <td> {{ p.name }} </td>
+                    <td> {{ p.amount }} </td>
+                    <td> {{ p.unitaryValue }} </td>
+                    <td> {{ p.discountValue }} </td>
+                    <td> {{ p.total }} </td>
+                </tr>
+                <div class="totalizer">
+                    <span>Total pedido: {{ this.totalRequest }}</span>
+                    <br>
+                    <span>Quantidade de itens: {{ this.request.products.length }}</span>
+                </div>
+            </tbody>
+        </table>
+        <div>   
+            <input class="btn btn-primary" type="button" value="Salvar">
+            <input class="btn btn-primary"  type="button" value="Salvar/Novo">
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,8 +48,19 @@ export default {
                 id: '',
                 emitDate: '',
                 client: '',
+                products: [
+                    {
+                        id: 0,
+                        name: 'Camiseta',
+                        amount: 10,
+                        unitaryValue: 10,
+                        discountValue: 1,
+                        total: 0,
+                    }
+                ],
             },
             isAdd: true,
+            totalRequest: 0,
         }
     },
     emits: [
@@ -21,15 +69,34 @@ export default {
     methods: {
 
     },
-    // mounted : {
-        
-    // },
+
+    mounted() {
+        if(this.request.products)
+        {
+            this.request.products.map(prod => {
+                prod.total = prod.amount * prod.unitaryValue - prod.discountValue;
+                this.totalRequest += prod.total;
+            });    
+        }
+    },
+
     components: {
         FormRequest
-    }
+    },
 }
 </script>
 
 <style>
-
+    .btnGroup {
+        margin: 10px;
+    }
+    .btn {
+        margin: 2px;
+    }
+    .totalizer {
+        align-content: initial;
+        text-align: right;
+        width: 375%;
+        padding-top: 10%;
+    }
 </style>
